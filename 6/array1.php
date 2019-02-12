275f8d8b4b9e4a3bf6dba228acaada26d8345731
akane-aa <kagemiyasenn@gmail.com>
2019-02-12 23:07:51 +0900

    add array file

---
+++ 6/array1.php
@@ -0,0 +1,9 @@
+<?php
+$array = array();
+
+$array[] = 'りんご';
+$array[] = 'みかん';
+$array[] = 'バナナ';
+
+echo $array[2];
+ ?>
---
+++ 6/array_other.php
@@ -0,0 +1,7 @@
+<?php
+$array1 = array();
+$array2 = array();
+
+$array1 = array('りんご','みかん','バナナ');
+$array2 = array('name' => '鈴木','hobby' => 'テニス','email' => 'sample@sample.com');
+var_dump($array2);
